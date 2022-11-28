79baabd7113b09c3b95d8ee963bfa4ccfdd193ea
Kenneth <alcanamay@gmail.com>
2022-11-28 07:32:42 -0600

    antes de valer veega
    aaaaa

--- Assets/Scripts/Weapons/DamageDemo_1.cs
+++ Assets/Scripts/Weapons/DamageDemo_1.cs
@@ -11,7 +11,7 @@ public class DamageDemo_1 : MonoBehaviour, ITargetCombat_1
     {
         damageFeedbackEffect.PlayDamageEffect();
         health -= damagePoints;
-        if (health == 0)
+        if (health <= 0)
         {
             Destroy(gameObject);
         }
